812adfdfaaf192926d562b416d62401e8b606a5f
Gleb Radchenko <glebradchenko@MacBook-Air-Gleb.local>
2017-01-09 00:01:12 +0200

    logic implementation

--- Photo Cropper/Helpers/LivePhotoPlayer.swift
+++ Photo Cropper/Helpers/LivePhotoPlayer.swift
@@ -22,7 +22,7 @@ protocol PlayerBehaviour {
     init(_ url: URL)
     func layer() -> AVPlayerLayer
     func move(to seconds: Double) throws
-    func captureImage(completion: @escaping (UIImage?, Error?) -> Void) throws
+    func captureImage(completion: @escaping (UIImage?, Error?) -> Void)
 }
 
 class LivePhotoPlayer: PlayerBehaviour {
@@ -55,13 +55,14 @@ class LivePhotoPlayer: PlayerBehaviour {
         guard let duration = duration, duration >= seconds else {
             throw LivePhotoPlayerError.wrongTime
         }
-        let time = CMTime(seconds: seconds, preferredTimescale: 10)
+        let time = CMTime(seconds: seconds, preferredTimescale: 1000)
         player.seek(to: time)
     }
     
-    func captureImage(completion: @escaping (UIImage?, Error?) -> Void) throws {
+    func captureImage(completion: @escaping (UIImage?, Error?) -> Void) {
         guard let generator = imageGenerator else {
-            throw LivePhotoPlayerError.emptyImageGenerator
+            completion(nil, LivePhotoPlayerError.emptyImageGenerator)
+            return
         }
         let currentTime = player.currentTime()
         generator.generateCGImagesAsynchronously(forTimes: [NSValue(time: currentTime)]) { (firstTime, image, secondTime, result, error) in
@@ -73,7 +74,68 @@ class LivePhotoPlayer: PlayerBehaviour {
                 completion(nil, error)
                 return
             }
-            completion(UIImage(cgImage: image), error)
+            var shouldRotate = false
+            if let videoSize = self.player.currentItem?.presentationSize {
+                if (videoSize.width > videoSize.height && image.width > image.height) ||
+                    (videoSize.width < videoSize.height && image.width < image.height) {
+                    shouldRotate = false
+                } else {
+                    shouldRotate = true
+                }
+            }
+            
+            var anImage = UIImage(cgImage: image)
+            if shouldRotate {
+                anImage = anImage.imageRotatedByDegrees(degrees: 90, flip: false)
+            }
+            completion(anImage, error)
         }
+    }
+}
+extension UIImage {
+    public func imageRotatedByDegrees(degrees: CGFloat, flip: Bool) -> UIImage {
+        let radiansToDegrees: (CGFloat) -> CGFloat = {
+            return $0 * (180.0 / CGFloat(M_PI))
+        }
+        let degreesToRadians: (CGFloat) -> CGFloat = {
+            return $0 / 180.0 * CGFloat(M_PI)
+        }
+        
+        // calculate the size of the rotated view's containing box for our drawing space
+        let rotatedViewBox = UIView(frame: CGRect(origin: .zero, size: size))
+        let t = CGAffineTransform(rotationAngle: degreesToRadians(degrees));
+        rotatedViewBox.transform = t
+        let rotatedSize = rotatedViewBox.frame.size
+        
+        // Create the bitmap context
+        UIGraphicsBeginImageContext(rotatedSize)
+        let bitmap = UIGraphicsGetCurrentContext()
+        
+        // Move the origin to the middle of the image so we will rotate and scale around the center.
+        bitmap?.translateBy(x: rotatedSize.width / 2.0, y: rotatedSize.height / 2.0);
+        
+        //   // Rotate the image context
+        bitmap?.rotate(by: degreesToRadians(degrees));
+        
+        // Now, draw the rotated/scaled image into the context
+        var yFlip: CGFloat
+        
+        if(flip){
+            yFlip = CGFloat(-1.0)
+        } else {
+            yFlip = CGFloat(1.0)
+        }
+        
+        bitmap?.scaleBy(x: yFlip, y: -1.0)
+        bitmap?.draw(cgImage!,
+                     in: CGRect(x: -size.width / 2,
+                                y:  -size.height / 2,
+                                width: size.width,
+                                height: size.height))
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
     }
 }
--- Photo Cropper/ViewControllers/CropperViewController.swift
+++ Photo Cropper/ViewControllers/CropperViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class CropperViewController: UIViewController {
     
+    lazy var livePhotoPickerController: LivePhotoPickerController = {
+        let picker = LivePhotoPickerController()
+        picker.delegate = self
+        return picker
+    }()
+    lazy var mediaManager: MediaResourceManager = {
+        return MediaResourceManager()
+    }()
+    var livePhotoPlayer: PlayerBehaviour?
+    
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var contentSlider: CustomSlider!
     
@@ -26,17 +36,66 @@ class CropperViewController: UIViewController {
     func setupUI() {
         
     }
-    
+    func showVideo(_ url: URL) {
+        livePhotoPlayer = LivePhotoPlayer(url)
+        guard let livePhotoPlayer = livePhotoPlayer else {
+            return
+        }
+        let layer = livePhotoPlayer.layer()
+        layer.frame = contentView.bounds
+        contentView.layer.addSublayer(layer)
+    }
     
     //MARK: - Actions
     
+    @IBAction func didTapSelectButton(_ sender: Any) {
+        //hide button 
+        present(livePhotoPickerController.picker, animated: true) {
+            //hide button
+        }
+    }
     @IBAction func actionDidTapShareButton(_ sender: Any) {
-        
+        guard let player = livePhotoPlayer else {
+            //handle somehow
+            return
+        }
+
+        player.captureImage { (image, error) in
+            if let image = image {
+                let activityController = SharingManager.activityController(with: image)
+                self.present(activityController, animated: true, completion: nil)
+            } else {
+                print(error)
+            }
+        }
     }
-    
     @IBAction func actionSliderValueChanged(_ sender: CustomSlider) {
-        print("current progress is \(sender.progress)")
+        guard let livePhotoPlayer = livePhotoPlayer, let duration = livePhotoPlayer.duration else {
+            return
+        }
+        let neededTime = Double(sender.progress) * duration
+        do {
+            try livePhotoPlayer.move(to: neededTime)
+            print(neededTime)
+        } catch {
+            print(error)
+        }
     }
     
-    
+}
+extension CropperViewController: LivePhotoPickerControllerDelegate {
+    func pickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        mediaManager.process(info) { [weak self] (url, error) in
+            if let url = url {
+                self?.showVideo(url)
+            } else {
+                //process error
+            }
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    func pickerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+        //show button
+    }
 }
