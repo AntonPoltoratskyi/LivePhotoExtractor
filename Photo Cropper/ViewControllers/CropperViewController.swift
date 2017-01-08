2937fe6fdc9e9771cff8f9b6bb10f950d560b747
Anton Poltoratskyi <anton.poltoratskii@gmail.com>
2017-01-08 21:18:55 +0200

    Add CropperViewController stub

---
+++ Photo Cropper/ViewControllers/CropperViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CropperViewController.swift
+//  Photo Cropper
+//
+//  Created by Anton Poltoratskyi on 08.01.17.
+//  Copyright © 2017 Gleb Radchenko. All rights reserved.
+//
+
+import UIKit
+
+class CropperViewController: UIViewController {
+    
+    
+    //MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+}
