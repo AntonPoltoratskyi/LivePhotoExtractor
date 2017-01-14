52cbb86de05f98c6da125e489d1317d2413ed667
Gleb Radchenko <glebradchenko@MacBook-Air-Gleb.local>
2017-01-14 14:12:06 +0200

    look at this idea

--- Photo Cropper/Helpers/TransitionAnimator.swift
+++ Photo Cropper/Helpers/TransitionAnimator.swift
@@ -31,43 +31,49 @@ class TransitionAnimator: UIPercentDrivenInteractiveTransition, UIViewController
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
         return animationDuration
     }
+    
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-        let containerView = transitionContext.containerView
-        let toView = transitionContext.view(forKey: .to)!
-        let mainView = presenting ? toView : transitionContext.view(forKey: .from)!
-        
-        let initialFrame = presenting ? originFrame : mainView.frame
-        let finalFrame = presenting ? mainView.frame : originFrame
-        
-        var scaleX: CGFloat = initialFrame.width / finalFrame.width
-        scaleX = presenting ? scaleX : 1 / scaleX
-        var scaleY: CGFloat = initialFrame.height / finalFrame.height
-        scaleY = presenting ? scaleY : 1 / scaleY
-        
-        let scaling = CGAffineTransform(scaleX: scaleX, y: scaleY)
-        
-        if presenting {
-            toView.transform = scaling
-            toView.center = CGPoint(x: originFrame.midX, y: originFrame.midY)
-            toView.clipsToBounds = true
-        }
-        containerView.addSubview(toView)
-        containerView.bringSubview(toFront: mainView)
-        UIView.animate(withDuration: animationDuration,
-                       delay: 0,
-                       options: .curveEaseOut,
-                       animations: { 
-                        mainView.transform = self.presenting ? CGAffineTransform.identity : scaling
-                        mainView.center = CGPoint(x: finalFrame.midX, y: finalFrame.midY)
-        }) { (finished) in
-            transitionContext.completeTransition(finished)
+        print("CALL 1")
+        if transitionContext.transitionWasCancelled {
+            transitionContext.completeTransition(false)
+        } else {
+            let containerView = transitionContext.containerView
+            let toView = transitionContext.view(forKey: .to)!
+            let mainView = presenting ? toView : transitionContext.view(forKey: .from)!
+            
+            let initialFrame = presenting ? originFrame : mainView.frame
+            let finalFrame = presenting ? mainView.frame : originFrame
+            
+            var scaleX: CGFloat = initialFrame.width / finalFrame.width
+            scaleX = presenting ? scaleX : 1 / scaleX
+            var scaleY: CGFloat = initialFrame.height / finalFrame.height
+            scaleY = presenting ? scaleY : 1 / scaleY
+            
+            let scaling = CGAffineTransform(scaleX: scaleX, y: scaleY)
+            
+            if presenting {
+                toView.transform = scaling
+                toView.center = CGPoint(x: originFrame.midX, y: originFrame.midY)
+                toView.clipsToBounds = true
+            }
+            containerView.addSubview(toView)
+            containerView.bringSubview(toFront: mainView)
+            UIView.animate(withDuration: animationDuration,
+                           delay: 0,
+                           options: .curveEaseOut,
+                           animations: {
+                            mainView.transform = self.presenting ? CGAffineTransform.identity : scaling
+                            mainView.center = CGPoint(x: finalFrame.midX, y: finalFrame.midY)
+            }) { (finished) in
+                transitionContext.completeTransition(finished)
+            }
         }
     }
     
     
     
     func handlePanGesture(pan: UIPanGestureRecognizer) {
-        
+        print("CALL 2")
         let translation = pan.translation(in: pan.view)
         
         let percentageComplete: CGFloat = abs(translation.y / pan.view!.bounds.height)
@@ -79,9 +85,9 @@ class TransitionAnimator: UIPercentDrivenInteractiveTransition, UIViewController
             self.sourceViewController.dismiss(animated: true, completion: nil)
         case .changed:
             self.update(percentageComplete)
+            sourceViewController.view.alpha = 1 - percentageComplete
         default:
             self.isInteractive = false
-            self.finish()
             if percentageComplete > 0.5 {
                 self.finish()
             } else {
@@ -119,6 +125,7 @@ extension MainViewController: UIViewControllerTransitioningDelegate {
         animator.presenting = true
         return animator
     }
+    
     func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         animator.presenting = false
         return animator
