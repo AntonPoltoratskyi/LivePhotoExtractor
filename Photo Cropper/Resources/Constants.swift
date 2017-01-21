9c4669e62dc2daebd905278f6bd064f6f55a7dbe
Gleb Radchenko <glebradchenko@MacBook-Air-Gleb.local>
2017-01-21 12:35:51 +0200

    changed font of watermark

--- Photo Cropper/Resources/Constants.swift
+++ Photo Cropper/Resources/Constants.swift
@@ -18,7 +18,7 @@ class AppBundle {
 struct Constants {
     static let watermarkText = "#Snaplive"
     static let watermarkFont: UIFont = {
-        return UIFont.boldSystemFont(ofSize: 60.0)
+        return UIFont(name: "HelveticaNeue", size: 60)!
     }()
     static let watermarkColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.7)
 }
